4d90066b7f11027394a5784044307b62fef5ea8b
Njibhu <manu@njibhu.eu>
2019-07-06 19:19:39 +0200

    Update dependencies

--- gulpfile.js
+++ gulpfile.js
@@ -1,69 +1,76 @@
-'use strict';
+"use strict";
 
-var version = require('./package.json').version;
+const version = require("./package.json").version;
 
-var browserify = require('browserify');
-var gulp = require('gulp');
-var source = require('vinyl-source-stream');
-var buffer = require('vinyl-buffer');
-var sourcemaps = require('gulp-sourcemaps');
-var concat = require('gulp-concat');
-var log = require('gulplog');
-var sass = require('gulp-sass');
-var rename = require('gulp-rename');
-var uglifyjs = require('uglify-es');
-var composer = require('gulp-uglify/composer');
-var uglify = composer(uglifyjs, console);
-var watch = require('gulp-watch');
-var batch = require('gulp-batch');
+const browserify = require("browserify");
+const gulp = require("gulp");
+const source = require("vinyl-source-stream");
+const buffer = require("vinyl-buffer");
+const sourcemaps = require("gulp-sourcemaps");
+const concat = require("gulp-concat");
+const log = require("gulplog");
+const sass = require("gulp-sass");
+const rename = require("gulp-rename");
+const uglifyjs = require("uglify-es");
+const composer = require("gulp-uglify/composer");
+const uglify = composer(uglifyjs, console);
+const watch = require("gulp-watch");
+const batch = require("gulp-batch");
 
-gulp.task('t3dapp', function () {
-    // set up the browserify instance on a task basis
-    var b = browserify({
-        entries: './src/Tyria3DApp.js',
-        glob: './src/**/*.js',
-        debug: true
-    });
+gulp.task("default", function() {
+  // set up the browserify instance on a task basis
+  let b = browserify({
+    entries: "./src/Tyria3DApp.js",
+    glob: "./src/**/*.js",
+    debug: true
+  });
 
-    return b.bundle()
-        .pipe(source(`T3DAPP-${version}.min.js`))
-        .pipe(buffer())
-        .pipe(sourcemaps.init({loadMaps: true}))
-            // Add transformation tasks to the pipeline here.
-            .pipe(uglify())
-            .on('error', log.error)
-        .pipe(sourcemaps.write('./'))
-        .pipe(gulp.dest('./dist/build/'));
+  return (
+    b
+      .bundle()
+      .pipe(source(`T3DAPP-${version}.min.js`))
+      .pipe(buffer())
+      .pipe(sourcemaps.init({ loadMaps: true }))
+      // Add transformation tasks to the pipeline here.
+      .pipe(uglify())
+      .on("error", log.error)
+      .pipe(sourcemaps.write("./"))
+      .pipe(gulp.dest("./dist/build/"))
+  );
 });
 
-gulp.task('three-bundle', function(){
-    return gulp.src([
-        './dist/vendor/three/three.js',
-        './dist/vendor/three/DDSLoader.js',
-        './dist/vendor/three/FirstPersonControls.js',
-        './dist/vendor/three/OBJLoader.js',
-        './dist/vendor/three/OrbitControls.js',
-        './dist/vendor/three/PointerLockControls.js',
-        './dist/vendor/three/Projector.js',
-        './dist/vendor/three/Raycaster.js',
-        './dist/vendor/three/Stats.js',
-        './dist/vendor/three/TrackballControls.js'])
-    .pipe(concat({ path: 'three-bundle.js'}))
+gulp.task("three-bundle", function() {
+  return gulp
+    .src([
+      "./dist/vendor/three/three.js",
+      "./dist/vendor/three/DDSLoader.js",
+      "./dist/vendor/three/FirstPersonControls.js",
+      "./dist/vendor/three/OBJLoader.js",
+      "./dist/vendor/three/OrbitControls.js",
+      "./dist/vendor/three/PointerLockControls.js",
+      "./dist/vendor/three/Projector.js",
+      "./dist/vendor/three/Raycaster.js",
+      "./dist/vendor/three/Stats.js",
+      "./dist/vendor/three/TrackballControls.js"
+    ])
+    .pipe(concat({ path: "three-bundle.js" }))
     .pipe(uglify())
-    .pipe(gulp.dest('./dist/build/vendor'));
+    .pipe(gulp.dest("./dist/build/vendor"));
 });
 
-gulp.task("sass", function () {
-    return gulp.src('./dist/css/sass/main.scss')
-        .pipe(sass().on('error', sass.logError))
-        .pipe(rename('__3d.css'))
-        .pipe(gulp.dest('./dist/css'));
+gulp.task("sass", function() {
+  return gulp
+    .src("./dist/css/sass/main.scss")
+    .pipe(sass().on("error", sass.logError))
+    .pipe(rename("__3d.css"))
+    .pipe(gulp.dest("./dist/css"));
 });
 
-gulp.task('watch', function () {
-    watch('./src/**/*.js', batch(function (events, done) {
-        gulp.start('t3dapp', done);
-    }));
+gulp.task("watch", function() {
+  watch(
+    "./src/**/*.js",
+    batch(function(events, done) {
+      gulp.start("t3dapp", done);
+    })
+  );
 });
-
-gulp.task('default',['t3dapp']);
